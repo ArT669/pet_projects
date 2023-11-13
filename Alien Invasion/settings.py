dc55a86f261c287befc6496d51cdb8ef34ce1419
ArT669 <120614279+ArT669@users.noreply.github.com>
2023-11-13 19:55:09 +0700

    update

--- Alien Invasion/settings.py
+++ Alien Invasion/settings.py
@@ -18,7 +18,7 @@ class Settings():
 		self.bullet_width = 3
 		self.bullet_height = 15
 		self.bullet_color = (60, 60, 60)
-		self.bullets_allowed = 3
+		self.bullets_allowed = 3 # Сколько пуль может быть одновременно на экране
 
 		# Настройки пришельцев
 		self.alien_speed = 0.5 #x
